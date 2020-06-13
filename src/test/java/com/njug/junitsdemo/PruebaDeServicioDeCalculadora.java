04edfac2226c637d31b31c744d3c7e044cc910af
Francisco Contreras <fcontreras@giniem.com>
2020-06-12 23:33:13 -0600

    Se hicieron las pruebas del historial de servicios

--- src/test/java/com/njug/junitsdemo/PruebaDeServicioDeCalculadora.java
+++ src/test/java/com/njug/junitsdemo/PruebaDeServicioDeCalculadora.java
@@ -1,27 +1,48 @@
 package com.njug.junitsdemo;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.LinkedList;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(BlockJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(properties = { "logging.level.org.springframework=ERROR" })
 public class PruebaDeServicioDeCalculadora {
 
-    ServicioDeCalculadora objetoDePrueba = new ServicioDeCalculadora();
+    @Autowired
+    private ServicioDeCalculadora objetoDePrueba;
 
     @Before
     public void antesDeCadaPrueba() {
-        System.out.println("Antes de cada prueba");
+        System.out.println(">> Antes de cada prueba");
+        objetoDePrueba.historial = new LinkedList<String>();
+    }
+
+    @After
+    public void despuesDeCadaPrueba() {
+        System.out.println("<< Despues de cada prueba");
+    }
+
+    @BeforeClass
+    public static void antesDeLaClase() {
+        System.out.println("*** PruebaDeServicioDeCalculadora ***");
+    }
+
+    @AfterClass
+    public static void despuesDeLaClase() {
+        System.out.println("*** ^^^^^^ ***");
     }
 
     @Test
     public void pruebaDeSuma() {
+        System.out.println("pruebaDeSuma");
         //Dado que tenemos dos enteros
         int a = 3;
         int b = 4;
@@ -31,10 +52,22 @@ public class PruebaDeServicioDeCalculadora {
 
         //Verificamos
         assertEquals(7, resultado);
+        assertEquals(1, objetoDePrueba.historial.size());
     }
 
-    @After
-    public void despuesDeLasPruebas() {
-        System.out.println("Despues de cada prueba");
+    @Test
+    public void pruebaDeSumaDeDosNegativos() {
+        System.out.println("pruebaDeSumaDeDosNegativos");
+        //Dado que tenemos dos enteros
+        int a = -3;
+        int b = -4;
+
+        //Cuando ejecutamos la suma
+        int resultado = objetoDePrueba.sumar(a, b);
+
+        //Verificamos
+        assertEquals(-7, resultado);
+        assertEquals(1, objetoDePrueba.historial.size());
+
     }
 }
