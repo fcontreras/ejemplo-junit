f08825c687a1f94904d144e4cf6de1e660a13d3c
Francisco Contreras <fcontreras@giniem.com>
2020-06-12 22:29:45 -0600

    Se agrego historial al servicio de calculadora

--- src/main/java/com/njug/junitsdemo/ServicioDeCalculadora.java
+++ src/main/java/com/njug/junitsdemo/ServicioDeCalculadora.java
@@ -2,11 +2,18 @@ package com.njug.junitsdemo;
 
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedList;
+import java.util.List;
+
 @Service
 public class ServicioDeCalculadora {
 
+    List<String> historial = new LinkedList<String>();
+
     public int sumar(final int a, final int b) {
-        return a + b;
+        int resultado = a + b;
+        historial.add(String.format("Suma: %d + %d = %d", a, b, resultado));
+        return resultado;
     }
 
 }
